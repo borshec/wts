7adea0bfc9b1e9f197e7ea5383c3b132b03de258
borshec <young.specialist@gmail.com>
2018-01-19 22:02:53 +0300

    Added migrations to commit

--- datasheets/models.py
+++ datasheets/models.py
@@ -5,25 +5,27 @@ import datetime
 class CommonFields(models.Model):
     creation_date = models.DateTimeField(default=datetime.datetime.now)
     description = models.CharField(max_length=300)
-    # TODO add user field to common model
+    # TODO: add user field to common model
 
     class Meta:
         abstract = True
 
 
-class DatasheetsPackages(models.Model, CommonFields):
-    name = models.CharField(max_length=31)
-    description = models.CharField(max_length=150)
-    consist_of_files = models.ManyToManyField(Datasheets)
-
-
-class Datasheets(models.Model, CommonFields):
-    initial_filename = models.CharField(max_length=120)
-    file = models.FileField()
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=PROTECT)
-
-
-class Manufacturer(models.Model, CommonFields):
+class Manufacturer(CommonFields):
     full_name = models.CharField(max_length=120)
     short_name = models.CharField(max_length=10)
+
+
+class Datasheet(CommonFields):
+    initial_filename = models.CharField(max_length=120)
+    file = models.FileField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
+    # TODO: Add uuid field with uniqueness
+
+class DatasheetsPackage(CommonFields):
+    name = models.CharField(max_length=31)
+    consist_of_files = models.ManyToManyField(Datasheet)
+
+
+
 
