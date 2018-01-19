10a499e9fcc41601610cca75355dd5c201184f3f
borshec <young.specialist@gmail.com>
2018-01-19 22:02:53 +0300

    After compelling to work

--- datasheets/admin.py
+++ datasheets/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Datasheet, DatasheetsPackage, Manufacturer
 
+admin.site.register(Datasheet)
 # Register your models here.
--- wts/settings.py
+++ wts/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "datasheets"
+    "datasheets",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
