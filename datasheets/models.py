bfa1d870bdc951849af27dab81d4181f509aadaf
borshec <young.specialist@gmail.com>
2018-01-21 19:58:15 +0300

    Games with models

--- datasheets/models.py
+++ datasheets/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-import datetime,
+import datetime, hashlib, random
+from django.core.files import File
+from uuid import uuid4
+
+
+def random_byte_value():
+    return str(uuid4()).encode()
+
+def file_hash(file):
+    f = File(file)
+    hash_obj = hashlib.sha1()
+    for chunk in f.chunks():
+        hash_obj.update(chunk)
+    return hash_obj.digest()
+
+def rename_file(file):
 
 
 class CommonFields(models.Model):
-    creation_date = models.DateTimeField(default=datetime.datetime.now)
+    creation_date = models.DateTimeField(default=datetime.datetime.now, editable=False)
     description = models.CharField(max_length=300)
     # TODO: add user field to common model
 
@@ -16,16 +31,24 @@ class Manufacturer(CommonFields):
     short_name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
-        return "{} ({})".format(self.short_name, self.full_name)
+        return "{} ({}, {})".format(self.id, self.short_name, self.full_name)
 
 
 class Datasheet(CommonFields):
-    initial_filename = models.CharField(max_length=120)
+    initial_filename = models.CharField(max_length=120, editable=False)
     file = models.FileField()
+    sha1_digest = models.BinaryField(unique=True, editable=False, default=random_byte_value)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return "{} ( {} )".format(self.id, self.file)
+
     def save(self, *args, **kwargs):
-        self.initial_filename =
+        self.sha1_digest=file_hash(self.file)
+        self.initial_filename = str(self.file)
+        print("TEST")
+        super().save(*args, **kwargs)
+
     # TODO: Change filenames on upload
     # TODO: Evaluate md5 on any other hash of file
     # TODO: Hash field must be unique
